ea1ce4cf118a3ccbde943b061f772c3aedf9f340
SushiKebab17 <suketsarya@gmail.com>
2022-10-30 11:19:17 +0000

    added day 17 file

---
+++ src/bin/17.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = advent_of_code_2020::input::input("17");
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(_input: &[String]) {}
+
+fn part_two(_input: &[String]) {}
