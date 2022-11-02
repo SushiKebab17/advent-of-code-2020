531f5ca7fd913be58896eb3e851ab53017333cfc
SushiKebab17 <suketsarya@gmail.com>
2022-11-02 21:27:54 +0000

    completed part 1

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = advent_of_code_2020::input::input("17");
@@ -11,6 +11,7 @@ fn part_one(input: &[String]) {
     for _ in 0..6 {
         amend_space(&mut active_cubes);
     }
+    println!("{}", active_cubes.len());
 }
 
 fn part_two(_input: &[String]) {}
@@ -27,24 +28,37 @@ fn parse(input: &[String]) -> HashSet<(isize, isize, isize)> {
     active_cubes
 }
 
-fn amend_space(space: &mut HashSet<(isize, isize, isize)>) {
+fn amend_space(active_cubes: &mut HashSet<(isize, isize, isize)>) {
     let mut neighbours_map = HashMap::new();
-    let mut new_space: HashSet<(isize, isize, isize)> = HashSet::new();
-    for coord in space.iter() {
+    for coord in active_cubes.iter() {
         for neighbour in get_neighbours(coord) {
-            neighbours_map.entry(neighbour).and_modify(|n| *n += 1).or_insert(1);
+            neighbours_map
+                .entry(neighbour)
+                .and_modify(|n| *n += 1)
+                .or_insert(1);
         }
     }
+    let mut new_actives: HashSet<(isize, isize, isize)> = HashSet::new();
+    for coord in neighbours_map.keys() {
+        if active_cubes.contains(coord)
+            && (neighbours_map[coord] == 2 || neighbours_map[coord] == 3)
+        {
+            new_actives.insert(*coord);
+        } else if !active_cubes.contains(coord) && neighbours_map[coord] == 3 {
+            new_actives.insert(*coord);
+        }
+    }
+    *active_cubes = new_actives;
 }
 
-fn get_neighbours(coord: &(isize, isize, isize)) -> [(isize, isize, isize); 26]{
-    let mut neighbours = [(0,0,0); 26];
+fn get_neighbours(coord: &(isize, isize, isize)) -> [(isize, isize, isize); 26] {
+    let mut neighbours = [(0, 0, 0); 26];
     let mut i = 0;
-    for x in 0..3 {
-        for y in 0..3 {
-            for z in 0..3 {
-                if x != 1 && y != 1 && z != 1 {
-                    neighbours[i] = (coord.0 + x - 1, coord.1 + y - 1, coord.2 + z - 1);
+    for x in -1..=1 {
+        for y in -1..=1 {
+            for z in -1..=1 {
+                if (x, y, z) != (0, 0, 0) {
+                    neighbours[i] = (coord.0 + x, coord.1 + y, coord.2 + z);
                     i += 1;
                 }
             }
