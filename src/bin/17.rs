efc3578bf27e2b5bcfafaf1808555b8db9c5d4df
Suket Arya <suketsarya@gmail.com>
2022-11-02 20:28:56 +0000

    created get_neighbours

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let input = advent_of_code_2020::input::input("17");
@@ -27,4 +27,28 @@ fn parse(input: &[String]) -> HashSet<(isize, isize, isize)> {
     active_cubes
 }
 
-fn amend_space(space: &mut HashSet<(isize, isize, isize)>) {}
+fn amend_space(space: &mut HashSet<(isize, isize, isize)>) {
+    let mut neighbours_map = HashMap::new();
+    let mut new_space: HashSet<(isize, isize, isize)> = HashSet::new();
+    for coord in space.iter() {
+        for neighbour in get_neighbours(coord) {
+            neighbours_map.entry(neighbour).and_modify(|n| *n += 1).or_insert(1);
+        }
+    }
+}
+
+fn get_neighbours(coord: &(isize, isize, isize)) -> [(isize, isize, isize); 26]{
+    let mut neighbours = [(0,0,0); 26];
+    let mut i = 0;
+    for x in 0..3 {
+        for y in 0..3 {
+            for z in 0..3 {
+                if x != 1 && y != 1 && z != 1 {
+                    neighbours[i] = (coord.0 + x - 1, coord.1 + y - 1, coord.2 + z - 1);
+                    i += 1;
+                }
+            }
+        }
+    }
+    neighbours
+}
