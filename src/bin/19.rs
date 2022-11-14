8309c0d8806464dc8e9eedd7efce706605c475da
Suket Arya <suketsarya@gmail.com>
2022-11-14 16:08:37 +0000

    added enum Rule in day 19

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -25,49 +25,65 @@ fn part_one(input: &[String]) {
 
 fn part_two(_input: &[String]) {}
 
-fn parse(input: &[String]) -> (HashMap<u32, Vec<&str>>, Iter<String>) {
+fn parse(input: &[String]) -> (HashMap<u32, Rule>, Iter<String>) {
     let mut lines = input.iter();
     let mut rules = HashMap::new();
     let mut line = lines.next().unwrap();
     while !line.is_empty() {
         let mut split = line.split(": ");
-        rules.insert(
-            split.next().unwrap().parse::<u32>().unwrap(),
-            split.next().unwrap().split(" | ").collect(),
-        );
+        let rule_num: u32 = split.next().unwrap().parse().unwrap();
+        let sub_rules = split.next().unwrap();
+        let rule = if sub_rules.starts_with("\"") {
+            Rule::Regex(
+                sub_rules
+                    .strip_prefix("\"")
+                    .unwrap()
+                    .strip_suffix("\"")
+                    .unwrap()
+                    .to_string(),
+            )
+        } else {
+            let mut patterns = Vec::new();
+            for pattern in sub_rules.split(" | ") {
+                let mut sub_patterns = Vec::new();
+                for sub_pat in pattern.split(" ") {
+                    sub_patterns.push(sub_pat.parse().unwrap());
+                }
+                patterns.push(sub_patterns);
+            }
+            Rule::SubRule(patterns)
+        };
+        rules.insert(rule_num, rule);
         line = lines.next().unwrap();
     }
     (rules, lines)
 }
 
-fn make_regex(map: &HashMap<u32, Vec<&str>>, rule: u32) -> String {
+fn make_regex(map: &HashMap<u32, Rule>, rule: u32) -> String {
     let mut regex = String::new();
-    let list = &map[&rule];
-    if list[0].starts_with("\"") {
-        return list[0]
-            .strip_prefix("\"")
-            .unwrap()
-            .strip_suffix("\"")
-            .unwrap()
-            .to_string();
-    } else if list.len() == 1 {
-        for sub_rule in list[0].split(" ") {
-            regex += "(";
-            regex += &make_regex(map, sub_rule.parse().unwrap());
-            regex += ")";
-        }
-    } else {
-        for sub_rule in list[0].split(" ") {
-            regex += "(";
-            regex += &make_regex(map, sub_rule.parse().unwrap());
-            regex += ")";
-        }
-        regex += "|";
-        for sub_rule in list[1].split(" ") {
-            regex += "(";
-            regex += &make_regex(map, sub_rule.parse().unwrap());
-            regex += ")";
+    let pattern = &map[&rule];
+    match pattern {
+        Rule::Regex(c) => return c.to_string(),
+        Rule::SubRule(list) => {
+            for &sub_rule in &list[0] {
+                regex += "(";
+                regex += &make_regex(map, sub_rule);
+                regex += ")";
+            }
+            if let Some(list_2) = list.get(1) {
+                regex += "|";
+                for &sub_rule in list_2 {
+                    regex += "(";
+                    regex += &make_regex(map, sub_rule);
+                    regex += ")";
+                }
+            }
         }
     }
     regex
+}
+
+enum Rule {
+    SubRule(Vec<Vec<u32>>),
+    Regex(String),
 }
